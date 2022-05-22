1de59cc3abc62b4aa6b32629baa0d48727112e8d
oLucasNascimento <lucas.nferreira@bandtec.com.br>
2022-05-22 17:39:11 -0300

    ManyToMany association with JoinTable

--- src/main/java/com/project/spring/config/TestConfig.java
+++ src/main/java/com/project/spring/config/TestConfig.java
@@ -40,6 +40,8 @@ public class TestConfig implements CommandLineRunner {
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
     
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+    
         Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
         Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
         Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
@@ -48,14 +50,21 @@ public class TestConfig implements CommandLineRunner {
         
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
-        
+    
+        userRepository.saveAll(Arrays.asList(u1, u2));
+    
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, u1);
         
-        categoryRepository.saveAll(Arrays.asList(cat1, cat2,cat3));
-        userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+        
+        p1.getCategories().add(cat2);
+        p2.getCategories().addAll(Arrays.asList(cat1,cat3));
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+        
         productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
         
     }
