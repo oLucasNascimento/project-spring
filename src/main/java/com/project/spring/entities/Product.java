053593a810608b4550fd77a1e2954929090a061e
oLucasNascimento <lucas.nferreira@bandtec.com.br>
2022-05-22 19:18:15 -0300

    Product-OrderItem OneToMany association

--- src/main/java/com/project/spring/entities/Product.java
+++ src/main/java/com/project/spring/entities/Product.java
@@ -1,6 +1,8 @@
 package com.project.spring.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
 import javax.persistence.*;
 import java.io.Serializable;
@@ -23,7 +25,10 @@ public class Product implements Serializable {
     
     @ManyToMany
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories = new HashSet();
+    private Set<Category> categories = new HashSet<>();
+    
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
     
     public Product() {
     }
@@ -78,6 +83,15 @@ public class Product implements Serializable {
     
     public Set<Category> getCategories() {
         return categories;
+    }
+    
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> orders = new HashSet<>();
+        for(OrderItem item : items) {
+            orders.add(item.getOrder());
+        }
+        return orders;
     }
     
     @Override
