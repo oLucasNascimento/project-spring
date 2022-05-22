cf0fe560dc78a5d0b9bc529b7c312e2b116f9dbb
oLucasNascimento <lucas.nferreira@bandtec.com.br>
2022-05-22 18:04:04 -0300

    rderItem, ManytoMany association with extra attributes

---
+++ src/main/java/com/project/spring/entities/pk/OrderItemPK.java
@@ -0,0 +1,51 @@
+package com.project.spring.entities.pk;
+
+import com.project.spring.entities.Order;
+import com.project.spring.entities.Product;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPK implements Serializable {
+    
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    
+    public Order getOrder() {
+        return order;
+    }
+    
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+    
+    public Product getProduct() {
+        return product;
+    }
+    
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPK orderItemPK = (OrderItemPK) o;
+        return Objects.equals(getOrder(), orderItemPK.getOrder()) && Objects.equals(getProduct(), orderItemPK.getProduct());
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(getOrder(), getProduct());
+    }
+}
