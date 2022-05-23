94330b72f50220601d624fd7b0d44aa48abe97f9
oLucasNascimento <lucas.nferreira@bandtec.com.br>
2022-05-23 12:32:27 -0300

    Exception handler - Delete

--- src/main/java/com/project/spring/services/UserService.java
+++ src/main/java/com/project/spring/services/UserService.java
@@ -2,8 +2,11 @@ package com.project.spring.services;
 
 import com.project.spring.entities.User;
 import com.project.spring.repositories.UserRepository;
+import com.project.spring.services.exceptions.DataBaseException;
 import com.project.spring.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,7 +22,7 @@ public class UserService {
         return this.userRepository.findAll();
     }
     
-    public User findById(Integer id){
+    public User findById(Integer id) {
         Optional<User> user = this.userRepository.findById(id);
         return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
@@ -28,8 +31,14 @@ public class UserService {
         return userRepository.save(user);
     }
     
-    public void deleteById(Integer id){
-        this.userRepository.deleteById(id);
+    public void deleteById(Integer id) {
+        try {
+            this.userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataBaseException(e.getMessage());
+        }
     }
     
     public User updateUser(Integer id, User user) {
---
+++ src/main/java/com/project/spring/services/exceptions/DataBaseException.java
@@ -0,0 +1,9 @@
+package com.project.spring.services.exceptions;
+
+public class DataBaseException extends RuntimeException {
+    
+    public DataBaseException(String message) {
+        super(message);
+    }
+    
+}
